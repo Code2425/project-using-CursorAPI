daf65b94da774afd2c47f11bad27fc09350da17c
Kesar Rawat <kesarawat@gmail.com>
2026-04-28 00:01:24 -0500

    Add comment to health check endpoint in HealthController

--- src/main/java/com/example/restapi/HealthController.java
+++ src/main/java/com/example/restapi/HealthController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthController {
 
+    // This is a health check endpoint
     @GetMapping("/health")
     public String health() {
         return "service is up";
